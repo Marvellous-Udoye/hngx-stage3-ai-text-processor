a1620d48c91a7caaca7cc3ad289665c1459edf61
Marv_lous <155150860+Marvellous-Udoye@users.noreply.github.com>
2025-02-18 07:15:08 +0100

    feat: build typewriter component for output text

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,5 @@
-
 export default function Sidebar() {
-  return <div></div>;
+  return (
+    <div className="h-screen flex flex-col justify-between py-6 px-4 border border-r-[#E2E8F0]"></div>
+  );
 }
---
+++ src/components/chat/Typewriter.tsx
@@ -0,0 +1,39 @@
+interface TypewriterProps {
+  text: string;
+  speed?: number;
+  className?: string;
+}
+
+import { useEffect, useState } from "react";
+
+const useTypewriter = (text: string, speed = 50) => {
+  const [displayText, setDisplayText] = useState("");
+
+  useEffect(() => {
+    let i = 0;
+    const typingInterval = setInterval(() => {
+      if (i < text.length) {
+        setDisplayText((prevText) => prevText + text.charAt(i));
+        i++;
+      } else {
+        clearInterval(typingInterval);
+      }
+    }, speed);
+
+    return () => {
+      clearInterval(typingInterval);
+    };
+  }, [text, speed]);
+
+  return displayText;
+};
+
+export default function Typewriter({
+  text,
+  speed,
+  className,
+}: TypewriterProps) {
+  const displayText = useTypewriter(text, speed);
+
+  return <p className={className}>{displayText}</p>;
+}
